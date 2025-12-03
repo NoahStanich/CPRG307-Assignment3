fa74b463f5f6c6a6361a7c5b000099b444af2598
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-03 14:32:49 -0700

    Added the other features, and removed print debugging

--- A3.sql
+++ A3.sql
@@ -10,7 +10,8 @@ start "C:\cprg307\Assignment 3 Part 1 Scripts\A3_test_dataset_1_-_Clean[1].sql";
 DECLARE
     CURSOR new_transactions_cursor IS
     SELECT account_no, transaction_no, transaction_type, transaction_amount, transaction_date, description
-    FROM new_transactions;
+    FROM new_transactions
+    FOR UPDATE;
 
     v_account_no new_transactions.account_no%TYPE;
     v_transaction_no new_transactions.transaction_no%TYPE;
@@ -19,9 +20,12 @@ DECLARE
     v_transaction_date new_transactions.transaction_date%TYPE;
     v_description new_transactions.description%TYPE;
     v_old_transaction_no new_transactions.transaction_no%TYPE;
+    v_account_balance account.account_balance%TYPE;
+    v_new_account_amount account.account_balance%TYPE;
 BEGIN
     OPEN new_transactions_cursor;
     DBMS_OUTPUT.PUT_LINE('test');
+    -- Percaution to not skip over details
     v_old_transaction_no := -1;
     LOOP
         FETCH new_transactions_cursor into v_account_no, v_transaction_no, v_transaction_type, v_transaction_amount, v_transaction_date, v_description;
@@ -35,11 +39,22 @@ BEGIN
         INSERT INTO transaction_detail(account_no, transaction_no, transaction_type, transaction_amount)
         VALUES (v_account_no, v_transaction_no, v_transaction_type, v_transaction_amount);
         
+        -- Grabs account balance
+        SELECT account_balance INTO v_account_balance FROM ACCOUNT
+        WHERE account_no = v_account_no;
+        -- Decides whether to add or subtract values from account
+        IF (v_transaction_type = 'D') THEN
+        v_new_account_amount := v_account_balance + v_transaction_amount;
+        ELSE
+        v_new_account_amount := v_account_balance - v_transaction_amount;
+        END IF;
         -- Updates the accounts
         UPDATE ACCOUNT
-        SET account_balance = v_transaction_amount + account_balance
+        SET account_balance = v_new_account_amount
         WHERE account_no = v_account_no;
         v_old_transaction_no := v_transaction_no;
+        -- Deletes the item after being processed
+        DELETE FROM NEW_TRANSACTIONS WHERE CURRENT OF new_transactions_cursor;
         
     END LOOP;
     CLOSE new_transactions_cursor;
